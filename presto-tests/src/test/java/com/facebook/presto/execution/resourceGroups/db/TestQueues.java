de048526d2c8f8df35965edf65f19cf4e7f86ef7
Nezih Yigitbasi <nezihy@fb.com>
2017-06-27 15:14:48 -0700

    Remove unused fields

--- presto-tests/src/test/java/com/facebook/presto/execution/resourceGroups/db/TestQueues.java
+++ presto-tests/src/test/java/com/facebook/presto/execution/resourceGroups/db/TestQueues.java
@@ -80,7 +80,6 @@ public class TestQueues
         String dbConfigUrl = getDbConfigUrl();
         H2ResourceGroupsDao dao = getDao(dbConfigUrl);
         try (DistributedQueryRunner queryRunner = createQueryRunner(dbConfigUrl, dao)) {
-            QueryManager queryManager = queryRunner.getCoordinator().getQueryManager();
             // submit first "dashboard" query
             QueryId firstDashboardQuery = createQuery(queryRunner, newDashboardSession(), LONG_LASTING_QUERY);
 
@@ -129,7 +128,6 @@ public class TestQueues
             QueryId firstDashboardQuery = createQuery(queryRunner, newDashboardSession(), LONG_LASTING_QUERY);
             QueryId secondDashboardQuery = createQuery(queryRunner, newDashboardSession(), LONG_LASTING_QUERY);
 
-            ImmutableSet<QueryState> queuedOrRunning = ImmutableSet.of(QUEUED, RUNNING);
             waitForQueryState(queryRunner, firstDashboardQuery, RUNNING);
             waitForQueryState(queryRunner, secondDashboardQuery, QUEUED);
         }
