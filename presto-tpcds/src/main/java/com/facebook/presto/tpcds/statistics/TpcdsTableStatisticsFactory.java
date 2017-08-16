97c2d16a8e6ecbe93fb22df6b8b23f833c8c42cb
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-08-16 12:43:00 +0200

    Return TPCDS statistics values in Presto internal representation

--- presto-tpcds/src/main/java/com/facebook/presto/tpcds/statistics/TpcdsTableStatisticsFactory.java
+++ presto-tpcds/src/main/java/com/facebook/presto/tpcds/statistics/TpcdsTableStatisticsFactory.java
@@ -33,6 +33,7 @@ import java.util.Optional;
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.Chars.trimSpacesAndTruncateToLength;
 import static com.facebook.presto.spi.type.DateType.DATE;
+import static com.facebook.presto.spi.type.Decimals.isShortDecimal;
 import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
 import static com.facebook.presto.spi.type.IntegerType.INTEGER;
 
@@ -91,7 +92,7 @@ public class TpcdsTableStatisticsFactory
         else if (type instanceof CharType) {
             return trimSpacesAndTruncateToLength(Slices.utf8Slice((String) tpcdsValue), type);
         }
-        else if (type.equals(BIGINT) || type.equals(INTEGER) || type.equals(DATE) || type instanceof DecimalType) {
+        else if (type.equals(BIGINT) || type.equals(INTEGER) || type.equals(DATE) || (type instanceof DecimalType && isShortDecimal(type))) {
             return ((Number) tpcdsValue).longValue();
         }
         else if (type.equals(DOUBLE)) {
